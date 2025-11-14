54d9d7c7b41fbea2e765934088465f2cb173f88b
unknown <abdoubakka@gmail.com>
2025-11-14 16:20:49 +0100

    FIX: Correct Clerk catch-all route structure

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <SignIn routing="path" path="/sign-in" />
+      <SignIn />
     </div>
   );
 }
